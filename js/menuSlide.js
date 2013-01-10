584e1c8637d9cb76f44f9fb421ac434385eee8ef
unknown <Marcel Safont@id28549.ub.edu>
2013-01-10 15:46:14 +0100

    afegit arrows i menu idiomes

--- js/menuSlide.js
+++ js/menuSlide.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
+
+/*variables amb els objectes del dom*/
  var pull = $('#boto');
- var menu = $('#main-nav');
+ var main_menu = $('#main-nav');
+ var lang_menu = $("#idiomes");
+ var lang_item = $("#idiomes ul li:first") 
  
+
+ /*menu principal amagat*/
  $(pull).on('click', function(e){
- 	var position = $(menu).position();
+ 	var position = $(main_menu).position();
  	if(position.left < 0){
  		e.preventDefault();
- 		$(menu).animate({left:0});
+ 		$(main_menu).animate({left:0});
  	}
  	else{
  		e.preventDefault();
- 		$(menu).animate({left:-205});
+ 		$(main_menu).animate({left:-205});
  	}
  });
+
+/*menu idiomes amagat*/
+$(lang_item).click(function(event){
+	event.preventDefault();
+	$(lang_menu).find("ul li").animate({opacity: 1}, 200);
+	
+})
+
 
 })
