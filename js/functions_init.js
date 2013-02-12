bad7118b211259fbf7bcf6003e342b06e9080e0f
marcel safont <marcel.safont1983@gmail.com>
2013-02-12 18:44:57 +0100

    afegit desde casa menu ok

--- js/functions_init.js
+++ js/functions_init.js
@@ -5,9 +5,7 @@ $(document).ready(function(){
 		'class': 'bg_gris',
 	}).prependTo('#page');
 
-	$('<div></div>', {
-		'class': 'bg_gris_menu',
-	}).prependTo('#page');
+	
 
 
 	/*menu idiomes*/
@@ -15,10 +13,12 @@ $(document).ready(function(){
 	$('#idiomes').addClass('js');
 	$('#idiomes li.selected').find('a').clone().prependTo('#page').addClass('langhide');
 	$('.langhide').click(function(){
+		$('#main-nav').css({'z-index':'0'})
 		$('#idiomes').slideToggle('fast', function(){
 			$('.bg_gris').toggleClass('active');
 		});
 		$(this).toggleClass('active');
+
 	});
 	
 
@@ -27,13 +27,21 @@ $(document).ready(function(){
 
 	$('#main-nav h1').addClass('mobil');
 	$('h1.mobil').click(function(){
+		$('#main-nav').css({'z-index':'20'})
 		$(this).next('ul').toggleClass('open');
 		$(this).toggleClass('open').promise().done(function(){
-    				$('.bg_gris_menu').toggleClass('active');
+    				$('.bg_gris').toggleClass('active');
 			});
 		
+	});
+
+	/*desabilitar el fons gris*/
+	$('.bg_gris').click(function(){
+		$(this).toggleClass('active');
+		$('h1.mobil').next('ul').removeClass('open');
+		$('h1.mobil').removeClass('open');
+		$('#idiomes').slideUp('fast');
 	})
-	
 	
 })
 
